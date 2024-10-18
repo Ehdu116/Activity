5f0257598a22ab4158c55de9599a38f60a3060ad
Ehdu116 <marasiganehdu60@gmail.com>
2024-10-18 15:24:47 +0800

    i added a small paragraph in home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
     return (
         <div className="home-container">
             <h1>Welcome to Marasigan Profile</h1>
-            <p>My career goal is to become a game developer.</p>
+            <p>My career goal is to become a game developer and be succesful!.</p>
         </div>
     );
 };
